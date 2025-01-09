d96952be2cae60563fbbaecfeeee20b60d32dbd6
kubaz <zurawikjakub@gmail.com>
2025-01-09 15:26:14 +0100

    End Round 2

--- Source/NetworkGameCDV/Private/MilitaryGameMode.cpp
+++ Source/NetworkGameCDV/Private/MilitaryGameMode.cpp
@@ -10,6 +10,17 @@ void AMilitaryGameMode::BackToLobby()
 		UWorld* World = GetWorld();
 		if (World)
 		{
+			// Inform all players to travel to the new map
+			for (FConstPlayerControllerIterator Iterator = World->GetPlayerControllerIterator(); Iterator; ++Iterator)
+			{
+				APlayerController* PlayerController = Iterator->Get();
+				if (PlayerController && PlayerController->IsLocalController() == false) // Ensure this is a client
+				{
+					PlayerController->ClientTravel(TEXT("/Game/Level/Lobby"), TRAVEL_Absolute);
+				}
+			}
+
+			// Server travels to the lobby
 			World->ServerTravel(TEXT("/Game/Level/Lobby"));
 		}
 	}
