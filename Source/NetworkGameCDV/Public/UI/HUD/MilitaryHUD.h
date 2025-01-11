2e90fe74b13abb31cf7d7750c82cc8b5660986ee
kubaz <zurawikjakub@gmail.com>
2025-01-11 18:35:22 +0100

    Add MilitaryHUD

---
+++ Source/NetworkGameCDV/Public/UI/HUD/MilitaryHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "MilitaryHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKGAMECDV_API AMilitaryHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
